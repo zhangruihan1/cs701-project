b4475830d3a03ada90ab2fe27f513853ac081ae6
Ubuntu <azureuser@vm1.1xhxntq54jfuzh5tmwqw1we3lb.ix.internal.cloudapp.net>
2022-10-24 14:30:28 +0000

    pretrain

--- test_l2g_voc.sh
+++ test_l2g_voc.sh
@@ -5,13 +5,13 @@ THR=0.25
 
 # CUDA_VISIBLE_DEVICES=0 python ./scripts/test_l2g_voc.py \
 #     --img_dir=../cs701/public/val_image/ \
-#     --test_list=../cs701/public/submission/label.txt \
+#     --test_list=../cs701/public/submission/label_.txt \
 #     --batch_size=1 \
 #     --dataset=pascal_voc \
 #     --input_size=224 \
 # 	--num_classes=20 \
 #     --thr=${THR} \
-#     --restore_from=./runs/${EXP}/model/pascal_voc_epoch_13.pth \
+#     --restore_from=./runs/${EXP}/model/pascal_voc_epoch_14.pth \
 #     --save_dir=./runs/${EXP}/${TYPE}/attention/ \
 #     --multi_scale \
 #     --cam_png=./runs/${EXP}/cam_png/
